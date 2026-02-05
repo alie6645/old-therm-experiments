0c0995f04a60ed55cdb246d32ffbd80d51d81d87
derpi <derpi@ACOMPUTER>
2026-02-04 22:12:55 -0700

    add inference

---
+++ infer.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import DataLoader
+from model import NeuralNetwork
+from ExperimentData import ExperimentDataset
+import cv2
+
+test_data = ExperimentDataset(
+"olddata\\tiny\\test\\rgb",
+"olddata\\tiny\\test\\therm",
+len=100
+)
+
+loader = DataLoader(test_data, batch_size=10, shuffle=True)
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+model = NeuralNetwork().to(device)
+print(model)
+
+torch.no_grad()
+model.eval()
+
+rgb, therm = next(iter(loader))
+pred = model(rgb)
+pred = torch.reshape(pred, (10, 1, 32, 32))
+therm = therm.squeeze()
+pred = pred.squeeze()
+
+cv2.imshow("rgb", rgb[0][0].numpy())
+cv2.waitKey(0)
+cv2.imshow("therm", therm[0].numpy())
+cv2.waitKey(0)
+cv2.imshow("pred", pred[0].detach().numpy())
+cv2.waitKey(0)
+print(pred[0])
+
