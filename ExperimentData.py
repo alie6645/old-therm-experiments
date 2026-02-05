91ef254451e0972fd04e0b60abe47e14d583a9fa
derpi <derpi@ACOMPUTER>
2026-02-04 21:19:45 -0700

    normalize tensors on retreival

--- ExperimentData.py
+++ ExperimentData.py
@@ -4,14 +4,15 @@ from torch.utils.data import Dataset
 from torchvision.io import decode_image
 
 class ExperimentDataset(Dataset):
-    def __init__(self, rgb_dir, therm_dir, transform=None, target_transform=None):
+    def __init__(self, rgb_dir, therm_dir, transform=None, target_transform=None, len=1000):
         self.rgb_dir = rgb_dir
         self.therm_dir = therm_dir
         self.transform = transform
         self.target_transform = target_transform
+        self.len = len
 
     def __len__(self):
-        return 1000
+        return self.len
 
     def __getitem__(self, idx):
         rgb_path = os.path.join(self.rgb_dir, str(idx) + ".jpg")
@@ -22,4 +23,7 @@ class ExperimentDataset(Dataset):
             rgbimage = self.transform(rgbimage)
         if self.target_transform:
             label = self.target_transform(label)
-        return rgbimage.float(), label.float()
+        div = 1.0/255.0
+        rgbimage = rgbimage.float()
+        label = label.float()
+        return rgbimage.apply_(lambda num: num*div), label.apply_(lambda num: num*div)
