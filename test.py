9b1cac513c9d378d5cdc324ae06f584988e0195f
derpi <derpi@ACOMPUTER>
2026-02-04 21:42:59 -0700

    update test to properly display images

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import cv2
+import numpy
 from ExperimentData import ExperimentDataset
 
 data = ExperimentDataset(
@@ -12,8 +13,12 @@ loader = DataLoader(data, batch_size=10, shuffle=True)
 
 rgb, therm = next(iter(loader))
 rgb = rgb.squeeze()
+rgbarr = rgb[0][0].numpy()
 therm = therm.squeeze()
-cv2.imshow("rgb", rgb[0][0].numpy())
+thermarr = therm[0].numpy()
+
+cv2.imshow("rgb", rgbarr)
 cv2.waitKey(0)
-cv2.imshow("therm", therm[0][0].numpy())
+cv2.imshow("therm", thermarr)
 cv2.waitKey(0)
+
